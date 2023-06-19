e00c525be0198522e40622ef0f0fe3947c87443e
Gordenguan <gordenguan122@gmail.com>
2023-06-18 23:20:42 -0600

    wordsearch function complete and test

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,41 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//return false if empty array
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return false;
+  }
+  //join letters together
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
 
-module.exports = wordSearch
+  //check if string includes word
+  for (let item of horizontalJoin) {
+    if (item.includes(word)) {
+      return true;
+    }
+  }
+  for (let item of verticalJoin) {
+    if (item.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+
+};
+//transpose matrix rows and columns switch
+const transpose = (letters) => {
+  const rowLength = letters[0].length;
+  const columnLength = letters.length;
+  const newArr = [];
+
+  for (let item of letters[0]) {
+    newArr.push([]);
+  }
+  for (let i = 0; i < columnLength; i++) {
+    for (let j = 0; j < rowLength; j++) {
+      newArr[j][i] = letters[i][j];
+    }
+  }
+  return newArr;
+};
+
+module.exports = wordSearch;
